6501cbe7d250cebf83b53adabab6442793531f54
Kiren <kiren@azurethailand.com>
2020-02-12 16:59:09 +0700

    setting up initial board game

--- src/boardGame/board.js
+++ src/boardGame/board.js
@@ -13,3 +13,23 @@ class Square extends React.Component {
         );
     }
 }
+
+class Game extends React.Component {
+    render() {
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info">
+                <div>{/*status*/}</div>
+                <ol>{/* todo */}</ol>
+            </div>
+        </div>
+
+    }
+}
+
+
+ReactDOM.render(
+    <Game />, document.getElementById('root')
+);
