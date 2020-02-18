1f53770664e31d6ff63d5316812c4f09e254f367
PaulKiren <kirenpaul@gmail.com>
2020-02-18 22:51:20 +0530

    Lifting up the state

--- src/boardGame/board.js
+++ src/boardGame/board.js
@@ -1,28 +1,32 @@
 import React from 'react';
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state= {
-            value: null,
-        };
-    }
     render() {
       return (
         <button className="square" 
-        onClick={()=>this.setState({value:'X'})}>
-          {this.state.value }
+        onClick={()=>this.props.onClick({value:'X'})}>
+          {this.props.value }
         </button>
       );
     }
   }
   
   class Board extends React.Component {
-    renderSquare(i) {
-        return <Square value={i} />
+      constructor(props) {
+          super(props);
+          this.state= {
+              squares:Array(9).fill(null),
           }
       }
+
+      handleClick(i) {
+          const squares = this.state.squares.slice();
+          squares[i]='X';
+          this.setState({squares:squares});
+      }
     renderSquare(i) {
-      return <Square value={i} />;
+      return <Square value={this.state.squares[i]} 
+                onClick={()=>this.handleClick(i)}
+      />;
     }
   
     render() {
