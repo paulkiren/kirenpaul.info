be67f3cd3b15bd0dd8e570a89aef926544c15ce6
PaulKiren <kirenpaul@gmail.com>
2020-02-18 22:36:11 +0530

    jsut add states and constructor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Game from './boardGame/board'
+
+
+
+ReactDOM.render(
+    <Game />, document.getElementById('root')
+);
 
