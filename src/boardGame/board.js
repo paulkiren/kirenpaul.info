15abd863ddc7e75c4740bd07cc6df241a30ca6ba
PaulKiren <kirenpaul@gmail.com>
2020-02-18 22:57:29 +0530

    square as function component

--- src/boardGame/board.js
+++ src/boardGame/board.js
@@ -1,14 +1,25 @@
 import React from 'react';
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square" 
-        onClick={()=>this.props.onClick({value:'X'})}>
-          {this.props.value }
-        </button>
-      );
-    }
-  }
+
+function Square (props) {
+    return (
+
+            <button className="square" 
+            onClick={()=>props.onClick}>
+              {props.value }
+            </button>
+        
+    );
+}
+// class Square extends React.Component {
+//     render() {
+//       return (
+//         <button className="square" 
+//         onClick={()=>this.props.onClick({value:'X'})}>
+//           {this.props.value }
+//         </button>
+//       );
+//     }
+//   }
   
   class Board extends React.Component {
       constructor(props) {
